6f3f3a5cf3ecfc33241cb7c4777356ae2e606ff9
Christoffel Angga <chris.ang.dev@gmail.com>
2021-01-15 13:49:01 -0800

    Implement all API call and interaction with database. tested with postman

--- database/mongodb.js
+++ database/mongodb.js
@@ -34,32 +34,8 @@ const qnaSchema = new mongoose.Schema({
 const Qna = mongoose.model('Qna', qnaSchema);
 const Answer = mongoose.model('Answer', answerSchema);
 
-Qna.find({})
-  .then((result) => console.log(result));
-
-// Qna.findById('6000f4af9bb92236ce7326a1')
-//   .then((result) => {
-//     // access subdocument for certain id
-//     console.log(result.answers.id('6000f4b19bb92236ce7327c6'));
-//   }).catch((error) => console.log(error));
-
-// console.log(faker.address.zipCodeByState());
-
-// this one is how to insert the question
-// Qna.create({ user: 'test', location: 'kasjdlkasd', email: 'asdasd' })
-//   .then((result) => {
-//     result.answers.push({ user: 'test', location: 'kasjdlkasd', email: 'asdasd' });
-//     result.answers.push({ user: 'test', location: 'kasjdlkasd', email: 'asdasd' });
-//     result.save();
-//     console.log(result);
-//   });
-// Qna.findById('600093a88dd8270b3a69bd24')
-//   .then((result) => {
-//     // this one is how to insert the answer in follow id
-//     result.answers.push({user: 'test', location: 'kasjdlkasd', email: 'asdasd'});
-//     result.save();
-//     console.log(result);
-//     console.log(faker.lorem.sentence());
-//   });
+// tester mongoose query sandbox
+// Qna.findById('6001e3b3bbe18d0925f61218')
+//   .then((result) => console.log(result));
 
 module.exports = Qna;
--- server/index.js
+++ server/index.js
@@ -7,9 +7,78 @@ const port = 3000;
 const Qna = require('../database/mongodb.js');
 
 app.use(express.static('public'));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/api/qna', (req, res) => {
+  Qna.find({ questionid: { $lte: 15, $gte: 0 } }).sort('questionid')
+    .then((result) => {
+      console.log(result);
+      res.send(result);
+    });
+});
+
+app.post('/api/qna', (req, res) => {
+  const {
+    user, location, email, body,
+  } = req.body;
+  Qna.findOne().sort('-questionid')
+    .then((result) => {
+      console.log(result.questionid);
+      Qna.create({
+        questionid: result.questionid + 1, user, location, email, body,
+      })
+        .then((created) => {
+          console.log(created);
+          res.send(created);
+        });
+    });
+});
+
+app.post('/api/qna/answer', (req, res) => {
+  const {
+    _id, user, location, email, body,
+  } = req.body;
+  Qna.findById(_id)
+    .then((result) => {
+      result.answers.push({
+        user, location, email, body,
+      });
+      result.save()
+        .then((created) => {
+          console.log(created);
+          res.send(created);
+        })
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
+});
+
+app.patch('/api/qna/answer/yes', (req, res) => {
+  const {
+    _id, _aid,
+  } = req.body;
+  Qna.findById(_id)
+    .then((result) => {
+      const currentDoc = result.answers.id(_aid);
+      currentDoc.yes += 1;
+      result.save();
+      res.send(result.answers.id(_aid));
+    })
+    .catch((err) => console.log(err));
+});
+
+app.patch('/api/qna/answer/no', (req, res) => {
+  const {
+    _id, _aid,
+  } = req.body;
+  Qna.findById(_id)
+    .then((result) => {
+      const currentDoc = result.answers.id(_aid);
+      currentDoc.no += 1;
+      result.save();
+      res.send(result.answers.id(_aid));
+    })
+    .catch((err) => console.log(err));
 });
 
 app.listen(port, () => {
