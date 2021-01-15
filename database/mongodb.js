b36f6cc28bf9bd8a164826a65df019084cba3453
Christoffel Angga <chris.ang.dev@gmail.com>
2021-01-14 18:27:30 -0800

    Seeding script done, Figure out how to access each subdoc by ID

--- database/mongodb.js
+++ database/mongodb.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
 const mongoose = require('mongoose');
+const faker = require('faker');
 
 mongoose.connect('mongodb://localhost/qna', { useNewUrlParser: true, useUnifiedTopology: true }, () => console.log('successfully connect to QnA DB'));
 mongoose.Promise = global.Promise;
@@ -30,16 +31,31 @@ const qnaSchema = new mongoose.Schema({
 });
 
 const Qna = mongoose.model('Qna', qnaSchema);
+const Answer = mongoose.model('Answer', answerSchema);
+
+Qna.findById('6000f4af9bb92236ce7326a1')
+  .then((result) => {
+    // access subdocument for certain id
+    console.log(result.answers.id('6000f4b19bb92236ce7327c6').location);
+  }).catch((error) => console.log(error));
+
+// console.log(faker.address.zipCodeByState());
 
 // this one is how to insert the question
 // Qna.create({ user: 'test', location: 'kasjdlkasd', email: 'asdasd' })
-//   .then((result) => console.log(result));
-Qna.findById('600093a88dd8270b3a69bd24')
-  .then((result) => {
-    // this one is how to insert the answer in follow id
-    // result.answers.push({user: 'test', location: 'kasjdlkasd', email: 'asdasd'});
-    // result.save();
-    console.log(result);
-  });
+//   .then((result) => {
+//     result.answers.push({ user: 'test', location: 'kasjdlkasd', email: 'asdasd' });
+//     result.answers.push({ user: 'test', location: 'kasjdlkasd', email: 'asdasd' });
+//     result.save();
+//     console.log(result);
+//   });
+// Qna.findById('600093a88dd8270b3a69bd24')
+//   .then((result) => {
+//     // this one is how to insert the answer in follow id
+//     result.answers.push({user: 'test', location: 'kasjdlkasd', email: 'asdasd'});
+//     result.save();
+//     console.log(result);
+//     console.log(faker.lorem.sentence());
+//   });
 
 module.exports = Qna;
